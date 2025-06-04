81f033c31bda15d79f37f7ea3e9569d897ffdb90
Balaj Javed <balajjaved1@gmail.com>
2025-06-04 22:29:00 +0500

    meta added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Portfolio | Muhammad Balaj Javed',
+  description: "Explore the portfolio of a passionate Web Developer and Software Engineering student, showcasing projects in React, Node.js, Firebase, and more.",
   generator: 'v0.dev',
 }
 
